b68bf96f4f8d3cc852d58ec4c67c0c71d25ef97a
Kevin Gomez <de.jesuskevin01@hotmail.com>
2021-04-09 21:39:40 -0400

    Primer commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const menu = document.querySelector('#menu');
+const menuBar = document.querySelector('#menu-bar');
+
+menuBar.addEventListener('click', ()=>{
+    menu.classList.toggle('menu-toggle');
+});
